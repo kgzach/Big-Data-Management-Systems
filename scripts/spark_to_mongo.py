e6f2c51067026fbe314ed2afdfdcc7e6e0f8e57b
El Maestro <kzachoulitis@gmail.com>
2024-10-20 22:00:40 +0300

    Update spark_to_mongo.py

--- scripts/spark_to_mongo.py
+++ scripts/spark_to_mongo.py
@@ -5,4 +5,9 @@ load_dotenv()
 
 kafka_broker = os.getenv('KAFKA_BROKER')
 db_path = os.getenv('DB_PATH')
+db_name = os.getenv('MONGO_DB_NAME')
+collection_name = os.getenv('MONGO_DB_COLLECTION')
 topic_name=os.getenv('TOPIC_NAME')
+
+def saveToMongo():
+    pass
